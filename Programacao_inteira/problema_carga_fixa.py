da7b73be43893dd2ca5a27fe22fd326b635b7dc2
Joscelino Oliveira <joscelino.oliveira@gmail.com>
2020-07-21 22:38:35 -0300

    #8 refatoracao do algoritmo do problema da carga fixa

--- Programacao_inteira/problema_carga_fixa.py
+++ Programacao_inteira/problema_carga_fixa.py
@@ -17,35 +17,33 @@ availability: Dict = {0: 30,
                       2: 78}
 
 # Decision variables
-var = LpVariable.dict("X", machines, cat='Integer', lowBound=0)
-var2 = LpVariable.dict("Y", machines, cat='Binary')
+var_1 = LpVariable.dict("X", machines, cat='Integer', lowBound=0)
+var_2 = LpVariable.dict("Y", machines, cat='Binary')
 
 # Model
 model = LpProblem("fixed_load", LpMinimize)
 
 # Goal function
-model += lpSum(var[i] * variable_cost[i] + var2[i] * fixed_cost[i] for i in machines)
+model += lpSum(var_1[i] * variable_cost[i] + var_2[i] * fixed_cost[i] for i in machines)
 
 # Constrains
 for i in machines:
-    model += var[i] <= availability[i] * var2[i]
-    
-constrains_list = []
-for x in var.values():
-    constrains_list.append(x)
-    
-model += lpSum(constrains_list) == 75
-    
+    model += var_1[i] <= availability[i] * var_2[i]
+
+model += lpSum(x for x in var_1.values()) == 75
+
+print(model)
 # Model solution
 status: int = model.solve()
-print(LpStatus[status])
-print(" ")
+
+print(f" *** Best configuration - {LpStatus[status]} *** ")
+print('-------------------------------------')
 print(f'The cost is: {round(value(model.objective), 2)}')
 print(" ")
 
 for i in machines:
-    print(f'{var[i]} = {value(var[i])}')
-    print(f'{var2[i]} = {value(var2[i])}')
+    print(f'{var_1[i]} = {value(var_1[i])}')
+    print(f'{var_2[i]} = {value(var_2[i])}')
 
 
 
